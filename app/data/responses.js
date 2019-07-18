3fd2ce84052e3e69d31dc19eba9241ee08b54f0a
Fabulous_Rachael <rachael.whitefield@gmail.com>
2019-07-18 12:39:52 -0600

    added responses.js and worked on apiRoutes functionality

---
+++ app/data/responses.js
@@ -0,0 +1,18 @@
+var responses = [
+    {
+        name: "Rachael",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStT889xooolcbRmUlDIN_t6WR2gT39y0RILWoHi42a8ThRIlMRvA",
+        scores: [
+            1,
+            2,
+            4,
+            5,
+            5,
+            3,
+            1,
+            2,
+            3,
+            3
+        ]
+    }
+]
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,11 +1,21 @@
 var path = require("path");
+var responses = require("../data/responses.js");
 var friends = require("../data/friends.js");
+
+
 module.exports = function(app) {
 
     app.get("/api/friends", function(req, res) {
-        res.json(friends)
+        res.send(friends);
     })
 
-    
+    app.get("api/responses", function(req, res) {
+        res.send(responses);
+    })
+
+    app.post("/api/response", function(req, res) {
+        responses.push(req.body);
+        res.send(true);
+    })
     
 }
