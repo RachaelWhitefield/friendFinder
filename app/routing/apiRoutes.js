9f2ef223b1a979a21f442e1e1e47d995a7ce51c9
Fabulous_Rachael <rachael.whitefield@gmail.com>
2019-07-19 19:30:52 -0600

    added math logic and for loop

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,13 +9,33 @@ module.exports = function(app) {
         res.send(friends);
     })
 
-    app.get("api/responses", function(req, res) {
-        res.send(responses);
-    })
+    app.post("/api/friends", function(req, res) {
+        var userScore = req.body.scores;
+        var scoresArr = [];
+        var bestMatch = 0;
 
-    app.post("/api/response", function(req, res) {
-        responses.push(req.body);
-        res.send(true);
-    })
+    for (var i = 0; i < friends.length; i++) {
+        var scoreDiff = 0;
+        for (var j = 0; j < userScore.length; j++) {
+            scoreDiff += (Math.abs(parseInt(friends[i].scores[j]) - parseInt(userScore[j])))
+        }
+        scoresArr.push(scoreDiff);
+    } 
+    
+    for (var i = 0; i < scoresArr.length; i++) {
+        if (scoresArr[i] <= scoresArr[bestMatch]) {
+            bestMatch = i;
+        }
+    }
+
+    var marvFriend = friends[bestMatch];
+    res.json(marvFriend);
+        friends.push(req.body);
+        // res.status(200).send(userScore);
+    });
     
 }
+
+
+    
+
